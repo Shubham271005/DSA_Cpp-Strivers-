9922e1351da6ff4ed85b9208099b27d7519b41a0
Shubham Jha <shubhamjha271005@gmail.com>
2025-02-15 19:50:01 +0530

    Update and rename 03_Arrays_and_Vectors to 03_Arrays_and_Vectors.cpp

--- Notes/03_Arrays_and_Vectors.cpp
+++ Notes/03_Arrays_and_Vectors.cpp
@@ -1,9 +1,10 @@
-Arrays
 
 # C++ Arrays Notes
 
 ## 1. Introduction to Arrays
-An **array** is a collection of elements of the same data type stored in contiguous memory locations.
+- An Array is a collection of elements of the same data type stored in contiguous memory locations.
+- You can access elements using their position (index) ( 0 to size-1 )
+- Array is linear
 
 ### **Why Use Arrays?**
 - Store multiple values under one variable name
@@ -13,31 +14,32 @@ An **array** is a collection of elements of the same data type stored in contigu
 ---
 
 ## 2. Declaration and Initialization
+
 ### **Syntax:**
 ```cpp
-<datatype> arrayName[size];
+ <datatype> arrayName[size];
 ```
 
 ### **Example:**
 ```cpp
-int arr[5]; // Declaration of an array of size 5
+ int arr[5]; // Declaration of an array of size 5
 ```
 
 ### **Initialization During Declaration:**
 ```cpp
-int arr[5] = {1, 2, 3, 4, 5};
-char vowels[5] = {'a', 'e', 'i', 'o', 'u'};
+ int arr[5] = {1, 2, 3, 4, 5};
+ char vowels[5] = {'a', 'e', 'i', 'o', 'u'};
 ```
 
-### **Omitting Size:**
+### **Omitting Size:** : Letting the compiler determine the array size
 ```cpp
-int arr[] = {1, 2, 3, 4, 5}; // Size automatically determined as 5
+  int arr[] = {1, 2, 3, 4, 5}; // Size automatically determined as 5
 ```
 
 ---
 
 ## 3. Accessing and Modifying Array Elements
-Elements are accessed using **zero-based indexing**:
+  Elements are accessed using **zero-based indexing**:
 ```cpp
 cout << arr[0]; // Accessing the first element
 arr[2] = 10;   // Modifying the third element
@@ -45,14 +47,15 @@ arr[2] = 10;   // Modifying the third element
 
 Looping through an array:
 ```cpp
-for(int i = 0; i < 5; i++) {
-    cout << arr[i] << " ";
+  for(int i = 0; i < 5; i++) {
+      cout << arr[i] << " ";
 }
 ```
 
 ---
 
 ## 4. Types of Arrays
+
 ### **1D Arrays**
 ```cpp
 int arr[3] = {10, 20, 30};
@@ -64,12 +67,12 @@ int matrix[2][3] = {{1, 2, 3}, {4, 5, 6}};
 ```
 Accessing elements:
 ```cpp
-cout << matrix[1][2]; // Outputs 6
+  cout << matrix[1][2]; // Outputs 6
 ```
 
 ### **3D Arrays**
 ```cpp
-int arr[2][2][3] = {{{1,2,3},{4,5,6}},{{7,8,9},{10,11,12}}};
+  int arr[2][2][3] = {{{1,2,3},{4,5,6}},{{7,8,9},{10,11,12}}};
 ```
 
 ---
