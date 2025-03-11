4e5e2ea23a9fc639413e4e781925006c947b2097
Shubham Jha <shubhamjha271005@gmail.com>
2025-03-11 14:54:48 +0530

    Update 04_STL_CPP.cpp

--- Notes/04_STL_CPP.cpp
+++ Notes/04_STL_CPP.cpp
@@ -14,87 +14,87 @@ It has four main components:
 #include <bits/stdc++.h>
 using namespace std;
 
-// ** Pairs **
-// void explainPairs()
-// {
-//     pair<int, int> p = {1, 3};
-//     cout << p.first << " " << p.second; // 1 3
+## ** Pairs **
+void explainPairs()
+{
+    pair<int, int> p = {1, 3};
+    cout << p.first << " " << p.second; // 1 3
 
-//     pair<int, pair<int, int>> p = {1, {3, 4}};
-//     cout << p.first << " " << p.second.second << " " << p.second.first; // 1 4 3
+    pair<int, pair<int, int>> p = {1, {3, 4}};
+    cout << p.first << " " << p.second.second << " " << p.second.first; // 1 4 3
 
-//     pair<int, int> arr[] = {{1, 2}, {2, 5}, {5, 1}};
-//     cout << arr[1].second; // 5
-// }
-
-// ** Vector **
+    pair<int, int> arr[] = {{1, 2}, {2, 5}, {5, 1}};
+    cout << arr[1].second; // 5
+}
+-----------------------------------------------------------
+## ** Vector **
 void explainVectors()
 {
-    // vector<int> v;
-    // v.push_back(1);
-    // v.emplace_back(2); // push_back and emplace_back works the same
+     vector<int> v;
+     v.push_back(1);
+    v.emplace_back(2); // push_back and emplace_back works the same
     // but generally emplace_back is faster than push_back()
 
-    // vector<pair<int, int>> vec;
-    // vec.push_back({1, 2});
-    // vec.emplace_back(1, 2);
+    vector<pair<int, int>> vec;
+    vec.push_back({1, 2});
+    vec.emplace_back(1, 2);
 
-    // vector<int> v(5, 100);
+    vector<int> v(5, 100); //  {100, 100, 100, 100, 100} ; 5 is size of v and 100 is the value to be stored in v
 
-    // vector<int> v(5);
+    vector<int> v(5); // {0, 0, 0, 0, 0}
 
-    // vector<int> v1(5, 10);
-    // vector<int> v2(v1);
+    vector<int> v1(5, 10); // {10, 10, 10, 10, 10}
+    vector<int> v2(v1); // {10, 10, 10, 10, 10} ; copy of v1
 
-    // iterators point to the memory address
+    iterators point to the memory address
 
-    // vector<int> v;
-    // v.push_back(10);
-    // v.push_back(20);
-    // v.push_back(30);
-    // v.push_back(50);
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(50);
 
-    // vector<int>::iterator it = v.begin();
-    // it++;
-    // cout << *(it) << " "; // 20
+    vector<int>::iterator it = v.begin();
+    it++;
+    cout << *(it) << " "; // 20
 
-    // it = it + 2;
-    // cout << *(it) << " "; // 50
+    it = it + 2;
+    cout << *(it) << " "; // 50
 
-    // vector<int>::iterator it = v.end();  // * end will not point to last elemnt
-    // it will point right after last element
+    vector<int>::iterator it = v.end();  // * end will not point to last elemnt
+    it will point right after last element
 
-    // vector<int>::iterator it = v.rend();
-    // vector<int>::iterator it = v.rbegin();
+    vector<int>::iterator it = v.rend();
+    vector<int>::iterator it = v.rbegin();
 
-    // cout << v[0] << " " << v.at(0) << " "; // 10 10
+    cout << v[0] << " " << v.at(0) << " "; // 10 10
 
-    // cout << v.back() << " "; // 50
+    cout << v.back() << " "; // 50
 
-    // for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
-    // {
-    //     cout << *(it) << " "; // 10 20 30 50
-    // }
+    for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
+    {
+        cout << *(it) << " "; // 10 20 30 50
+    }
 
-    // cout << endl;
+    cout << endl;
 
-    // for (auto it = v.begin(); it != v.end(); it++)
-    // {
-    //     cout << *(it) << " "; // 10 20 30 50
-    // }
+    for (auto it = v.begin(); it != v.end(); it++)
+    {
+        cout << *(it) << " "; // 10 20 30 50
+    }
 
-    // cout << endl;
+    cout << endl;
 
-    // for (auto it : v)
-    // {
-    //     cout << it << " "; // 10 20 30 50
-    // }
+    for (auto it : v)
+    {
+        cout << it << " "; // 10 20 30 50
+    }
 
-    // *** Deletion of vectors ***
-    // {10,20,30,50}
-    // v.erase(v.begin() + 1);
+    *** Deletion of vectors ***
+    {10,20,30,50}
+    v.erase(v.begin() + 1);
 
-    // v.erase(v.begin() + 1, v.begin() + 3); // [start,end)
+    v.erase(v.begin() + 1, v.begin() + 3); // [start,end)
 
     // *** Insert Function ***
 
